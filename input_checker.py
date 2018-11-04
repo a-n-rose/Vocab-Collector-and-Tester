09cac2cac899817c35e81b18d5c426f425599ffc
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-11-04 08:02:58 +0100

    removed unnecessary function

--- input_checker.py
+++ input_checker.py
@@ -6,11 +6,4 @@ def rem_space_specialchar(string):
         return string        
     return None
 
-def present_options_collect_response(options_list):
-    for item in options_list:
-        print(item)
-    input_answer = input()
-    return input_answer
     
-    
-#there is a problem in how the words of lists are identified - I think I did something funny in how I saved which list was active or how the list id is found... 
