3f29da6fbfe9801b6aeb06bccdaa1583145426aa
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2018-11-07 13:56:22 +0100

    added unittests for the input_manager.py module

---
+++ test_input_manager.py
@@ -0,0 +1,29 @@
+import unittest
+import input_manager
+
+
+class VocabInputCheck(unittest.TestCase):
+
+    def test_rem_space_specialchar_nospace_nospecialchar(self):
+        user_input = 'Stacey'
+        self.assertEqual(input_manager.rem_space_specialchar(user_input),'Stacey')
+        
+    def test_rem_space_specialchar_withspace_nospecialchar(self):
+        user_input = 'S t a c e y'
+        self.assertEqual(input_manager.rem_space_specialchar(user_input),'Stacey')
+        
+    def test_rem_space_specialchar_nospace_withspecialchar(self):
+        user_input = '$()(S?@ta______c&!e+=y'
+        self.assertEqual(input_manager.rem_space_specialchar(user_input),'Stacey')
+        
+    def test_rem_space_specialchar_noalphanumeric(self):
+        user_input = '*#(!)  (# #&___$$(@ '
+        self.assertEqual(input_manager.rem_space_specialchar(user_input),'')
+        
+    def test_rem_space_specialchar_empty(self):
+        user_input = ''
+        self.assertEqual(input_manager.rem_space_specialchar(user_input),'')
+    
+if __name__ == '__main__':
+    unittest.main()
+    
