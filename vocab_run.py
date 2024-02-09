63d6c768d487de8d95d72b99c36f5a3214035f18
Aislyn Rose <rose.aislyn.noelle@gmail.com>
2024-02-08 21:59:25 -0800

    Adapted code to updated package organization

--- vocab_run.py
+++ vocab_run.py
@@ -1,16 +1,15 @@
 
-from vocab_collector import Collect_Vocab
 import traceback
-from errors import ExitApp
+import vocab_lowkey as vlk
 
 if __name__ == "__main__":
     try:
-        user = Collect_Vocab('vocab_REAL.db')
+        user = vlk.vocab_manager.Collect_Vocab('vocab_test.db')
         user.sign_in()
         while user.is_user == True:
             user.action_list()
             
-    except ExitApp as e:
+    except vlk.errors.ExitApp as e:
         print(e)
     except Exception as e:
         print("Error Occurred: {}".format(e))
